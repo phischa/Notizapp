77fdbcfc60634e8bcf32238d4fa14bbbae96d612
phischa <ph.git@mailbox.org>
2024-01-15 23:14:52 +0100

    some script1

--- script.js
+++ script.js
@@ -6,13 +6,39 @@ function render() {
 }
 
 function addNote() {
+    let title = document.getElementById('input-title');
+    let note = document.getElementById('input-note');
+    titles.push(title.value);
+    notes.push(note.value);
 
+    save();
 }
 
 function deleteNote() {
 
 }
 
-function burgerMenu() {
-    
+function save() {
+    let titleAsText = JSON.stringify(titles);
+    let noteAsText = JSON.stringify(notes);
+    localStorage.setItem('titles', titleAsText);
+    localStorage.setItem('notes', noteAsText);
+}
+
+
+function load() {
+    let titleAsText = localStorage.getItem('titles');
+    let noteAsText = localStorage.getItem('notes');
+    if (titleAsText && noteAsText) {
+        titles = JSON.parse(titleAsText);
+        notes = JSON.parse(noteAsText);
+    }
+}
+
+function showMenu() {
+    document.getElementById('menu').classList.add('show-overlay-menu');
+}
+
+function hideMenu() {
+    document.getElementById('menu').classList.remove('show-overlay-menu');
 }
