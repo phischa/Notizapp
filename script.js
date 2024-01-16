4a8e6d5fa4ef367586a13b1f7b0fe15b6c056de6
phischa <ph.git@mailbox.org>
2024-01-16 17:56:30 +0100

    addNote()

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let titles = [];
 let notes = [];
 
-function render() {
+let deletedTitles = [];
+let deletedNotes = [];
 
+function render() {
+    let content = document.getElementById('content');
+    content.innerHTML = '';
+
+    for (let i = 0; i < titles.length; i++) {
+        content.innerHTML += /* html */ `
+            <div class="added-note">
+                <h3>${titles[i]} </h3> <br>
+                <div>${notes[i]}</div> <br>
+                <button class="delete-button" onclick="deleteNote(${i})">delete</button>
+            </div>
+        `;
+    }
 }
 
 function addNote() {
     let title = document.getElementById('input-title');
     let note = document.getElementById('input-note');
+
+    if (title.value === '' && note.value === '') {
+        alert('Beide Eingabefelder müssen ausgefüllt werden.')
+    } else {
     titles.push(title.value);
     notes.push(note.value);
-
+    }
+    render();
     save();
 }
 
